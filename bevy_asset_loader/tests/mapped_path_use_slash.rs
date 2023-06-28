1fe67b8ef5589001a70477dff290bc191d0c3845
Niklas Eicker <git@nikl.me>
2023-06-28 11:22:12 +0200

    Red test for #135

---
+++ bevy_asset_loader/tests/mapped_path_use_slash.rs
@@ -0,0 +1,57 @@
+#![allow(dead_code, unused_imports)]
+
+use bevy::app::AppExit;
+use bevy::asset::AssetPlugin;
+use bevy::audio::AudioPlugin;
+use bevy::prelude::*;
+use bevy::utils::HashMap;
+use bevy_asset_loader::asset_collection::AssetCollection;
+use bevy_asset_loader::loading_state::{LoadingState, LoadingStateAppExt};
+
+#[cfg(all(
+    not(feature = "2d"),
+    not(feature = "3d"),
+    not(feature = "progress_tracking")
+))]
+#[test]
+fn multiple_asset_collections() {
+    App::new()
+        .add_state::<MyStates>()
+        .add_plugins(MinimalPlugins)
+        .add_plugin(AssetPlugin::default())
+        .add_plugin(AudioPlugin::default())
+        .add_loading_state(LoadingState::new(MyStates::Load).continue_to_state(MyStates::Next))
+        .add_collection_to_loading_state::<_, AudioCollection>(MyStates::Load)
+        .add_system(timeout.run_if(in_state(MyStates::Load)))
+        .add_system(expect.in_schedule(OnEnter(MyStates::Next)))
+        .run();
+}
+
+fn timeout(time: Res<Time>) {
+    if time.elapsed_seconds_f64() > 60. {
+        panic!("The asset loader did not change the state in 60 seconds");
+    }
+}
+
+fn expect(collection: Option<Res<AudioCollection>>, mut exit: EventWriter<AppExit>) {
+    if collection.is_none() {
+        panic!("At least one asset collection was not inserted");
+    } else {
+        // make sure the asset paths use slash on all OS
+        assert!(collection.unwrap().files.contains_key("audio/plop.ogg"));
+        exit.send(AppExit);
+    }
+}
+
+#[derive(AssetCollection, Resource)]
+struct AudioCollection {
+    #[asset(path = "audio", collection(typed, mapped))]
+    files: HashMap<String, Handle<AudioSource>>,
+}
+
+#[derive(Clone, Eq, PartialEq, Debug, Hash, Default, States)]
+enum MyStates {
+    #[default]
+    Load,
+    Next,
+}
