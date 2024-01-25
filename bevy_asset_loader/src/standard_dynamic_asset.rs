4135f2e975c4f9f62173caff831f30ccea4b991e
Niklas Eicker <git@nikl.me>
2024-01-25 20:28:42 +0100

    Fix serialization of StandardDynamicAsset

--- bevy_asset_loader/src/standard_dynamic_asset.rs
+++ bevy_asset_loader/src/standard_dynamic_asset.rs
@@ -16,12 +16,10 @@ use bevy::sprite::TextureAtlas;
 
 #[cfg(any(feature = "3d", feature = "2d"))]
 use bevy::render::texture::{Image, ImageSampler, ImageSamplerDescriptor};
-#[cfg(any(feature = "3d", feature = "2d"))]
-use serde::Deserializer;
 
 /// These asset variants can be loaded from configuration files. They will then replace
 /// a dynamic asset based on their keys.
-#[derive(Debug, Clone, Deserialize, Serialize)]
+#[derive(Debug, Clone, Deserialize, Serialize, PartialEq)]
 #[serde(deny_unknown_fields)]
 pub enum StandardDynamicAsset {
     /// A dynamic asset directly loaded from a single file
@@ -49,7 +47,7 @@ pub enum StandardDynamicAsset {
         /// Image file path
         path: String,
         /// Sampler
-        #[serde(deserialize_with = "deserialize_some", default)]
+        #[serde(with = "optional", skip_serializing_if = "Option::is_none", default)]
         sampler: Option<ImageSamplerType>,
     },
     /// A dynamic standard material asset directly loaded from an image file
@@ -64,7 +62,7 @@ pub enum StandardDynamicAsset {
         /// Asset file path
         path: String,
         /// Sampler
-        #[serde(deserialize_with = "deserialize_some", default)]
+        #[serde(with = "optional", skip_serializing_if = "Option::is_none", default)]
         sampler: Option<ImageSamplerType>,
         /// The image width in pixels
         tile_size_x: f32,
@@ -75,34 +73,48 @@ pub enum StandardDynamicAsset {
         /// Rows on the sprite sheet
         rows: usize,
         /// Padding between columns in pixels
-        #[serde(deserialize_with = "deserialize_some", default)]
+        #[serde(with = "optional", skip_serializing_if = "Option::is_none", default)]
         padding_x: Option<f32>,
         /// Padding between rows in pixels
-        #[serde(deserialize_with = "deserialize_some", default)]
+        #[serde(with = "optional", skip_serializing_if = "Option::is_none", default)]
         padding_y: Option<f32>,
         /// Number of pixels offset of the first tile
-        #[serde(deserialize_with = "deserialize_some", default)]
+        #[serde(with = "optional", skip_serializing_if = "Option::is_none", default)]
         offset_x: Option<f32>,
         /// Number of pixels offset of the first tile
-        #[serde(deserialize_with = "deserialize_some", default)]
+        #[serde(with = "optional", skip_serializing_if = "Option::is_none", default)]
         offset_y: Option<f32>,
     },
 }
 
 #[cfg(any(feature = "3d", feature = "2d"))]
-fn deserialize_some<'de, D, G>(deserializer: D) -> Result<Option<G>, D::Error>
-where
-    D: Deserializer<'de>,
-    G: Deserialize<'de>,
-{
-    let opt: G = G::deserialize(deserializer)?;
-    Ok(Some(opt))
+mod optional {
+    use serde::{Deserialize, Deserializer, Serialize, Serializer};
+
+    pub(super) fn deserialize<'de, D, G>(deserializer: D) -> Result<Option<G>, D::Error>
+    where
+        D: Deserializer<'de>,
+        G: Deserialize<'de>,
+    {
+        let opt: G = G::deserialize(deserializer)?;
+        Ok(Some(opt))
+    }
+
+    pub(super) fn serialize<S, T>(value: &Option<T>, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        T: Serialize + std::fmt::Debug,
+        S: Serializer,
+    {
+        match value.as_ref() {
+            Some(value) => value.serialize(serializer),
+            None => serializer.serialize_none(),
+        }
+    }
 }
 
 /// Define the image sampler to configure for an image asset
 #[cfg(any(feature = "3d", feature = "2d"))]
-#[derive(Debug, Clone, Deserialize, Serialize)]
-#[serde(untagged)]
+#[derive(Debug, Clone, Deserialize, Serialize, PartialEq)]
 pub enum ImageSamplerType {
     /// See [`ImageSampler::nearest`]
     Nearest,
@@ -292,7 +304,7 @@ impl<K: Into<String> + Sync + Send + 'static> Command for RegisterStandardDynami
 ///
 /// These assets are loaded at the beginning of a loading state
 /// and combined in [`DynamicAssets`].
-#[derive(Deserialize, Serialize, Asset, TypePath)]
+#[derive(Deserialize, Serialize, Asset, TypePath, PartialEq, Debug)]
 pub struct StandardDynamicAssetCollection(pub HashMap<String, StandardDynamicAsset>);
 
 impl DynamicAssetCollection for StandardDynamicAssetCollection {
@@ -300,5 +312,50 @@ impl DynamicAssetCollection for StandardDynamicAssetCollection {
         for (key, asset) in self.0.iter() {
             dynamic_assets.register_asset(key, Box::new(asset.clone()));
         }
+    }
+}
+
+#[cfg(test)]
+#[cfg(feature = "2d")]
+mod tests {
+    use crate::prelude::StandardDynamicAssetCollection;
+
+    #[test]
+    fn serialize_and_deserialize_atlas() {
+        let dynamic_asset_file = r#"({
+    "texture_atlas": TextureAtlas(
+        path: "images/female_adventurer_sheet.png",
+        sampler: Nearest,
+        tile_size_x: 96.0,
+        tile_size_y: 99.0,
+        columns: 8,
+        rows: 1,
+        padding_x: 42.42,
+    ),
+})"#;
+        serialize_and_deserialize(dynamic_asset_file);
+    }
+
+    #[test]
+    fn serialize_and_deserialize_image() {
+        let dynamic_asset_file = r#"({
+    "image": Image(
+        path: "images/player.png",
+        sampler: Linear,
+    ),
+})"#;
+        serialize_and_deserialize(dynamic_asset_file);
+    }
+
+    fn serialize_and_deserialize(dynamic_asset_file: &'static str) {
+        let before: StandardDynamicAssetCollection = ron::from_str(dynamic_asset_file).unwrap();
+
+        let serialized_dynamic_asset_file =
+            ron::ser::to_string_pretty(&before, ron::ser::PrettyConfig::default()).unwrap();
+        assert_eq!(dynamic_asset_file, &serialized_dynamic_asset_file);
+
+        let after: StandardDynamicAssetCollection =
+            ron::from_str(&serialized_dynamic_asset_file).unwrap();
+        assert_eq!(before, after);
     }
 }
